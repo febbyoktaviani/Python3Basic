b5bae94cf9e56a9af191d9ed95909a2b0c54b6d4
febbyoktaviani <febby@julofinance.com>
2018-11-24 11:49:57 +0700

    initial commit

---
+++ demo_looping.py
@@ -0,0 +1,11 @@
+x = input('masukan angka pertama :')
+y = input('masukan angka kedua :')
+
+x = int(x)
+y = int(y)
+
+for i in range(0, x):
+    print(i)
+    for j in range(0, y):
+        print(j)
+
