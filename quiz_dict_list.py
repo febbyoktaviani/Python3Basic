d74b28c7cfad0e3f6a6308b5010a9caf636e2283
febby <febby@julofinance.com>
2018-12-01 16:32:52 +0700

    quiz dictionary and list

---
+++ quiz_dict_list.py
@@ -0,0 +1,25 @@
+tak hingga
+input: 5
+[1, 2, 3, 4 ,5]
+input: 3
+[2, 5, 6, 4, 5]
+input: 6
+[3, 7, 9, 8, 10, 6]
+input: 2
+[4, 9, 9, 8, 10, 6]
+input:9
+[5, 11, 12, 12, 15, 12, 7, 8, 9]
+ 1  2   3   4    5   6  7  8  9  10
+input: 10
+[6, 13, 15, 16, 20, 20, 14, 16, 18, 10]
+
+------------------------------
+bikin phonebook
+
+name: "febby"
+phone number: "081111956767"
+>> [{"name": "febby", phone number: "081111956767"}]
+name:
+phone number: ''
+>> 'Cannot enter Duplicate Data!!!!'
+[{"name": "febby", phone number: "081111956767"}, {'name': 'livia', 'phone number': '088812328'}]
